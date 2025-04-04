63d2163473082488a774527a47bebf631c55bc30
Student <Student@172.20.40.172>
2025-04-04 19:49:33 +0900

    0404

--- thisisjava/src/ch07/sec03/exam01/SmartPhone.java
+++ thisisjava/src/ch07/sec03/exam01/SmartPhone.java
@@ -3,7 +3,7 @@ package ch07.sec03.exam01;
 public class SmartPhone extends Phone {
 	//자식 생성자 선언
 	public SmartPhone(String model, String color) {
-		super();
+		super(); //부모클래스의 기본 생성자 
 		this.model = model;
 		this.color = color;
 		System.out.println("SmartPhone(String model, String color) 생성자 실행됨");
